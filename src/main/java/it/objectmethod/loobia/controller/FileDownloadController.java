6fba44999991d25e420ce3a1744f1118bdbab7bc
Andrea Verdiglione <andverdy@yahoo.it>
2020-05-06 11:18:22 +0200

    Controller download flat-file terminato.

---
+++ src/main/java/it/objectmethod/loobia/controller/FileDownloadController.java
@@ -0,0 +1,40 @@
+package it.objectmethod.loobia.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import it.objectmethod.loobia.service.FileDownloadService;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/download")
+public class FileDownloadController {
+
+	@Autowired
+	private FileDownloadService fileDownlService;
+
+	@GetMapping("/flat-file")
+	public void getSteamingFile1(HttpServletResponse response, HttpServletRequest request,
+			@RequestParam(value = "id") Integer id) throws IOException {
+		File file = fileDownlService.generateFile(id, request);
+		String fileNamePath = (String) request.getAttribute("fileName");
+
+		if (file != null) {
+			response.setContentType("application/txt");
+			response.setHeader("Content-Disposition", "attachment; filename=" + fileNamePath);
+			InputStream inputStream = new FileInputStream(file);
+
+			int nRead;
+			while ((nRead = inputStream.read()) != -1) {
+				response.getWriter().write(nRead);
+			}
+
+		}
+
+	}
+}
---
+++ src/main/java/it/objectmethod/loobia/service/FileDownloadService.java
@@ -0,0 +1,118 @@
+package it.objectmethod.loobia.service;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import it.objectmethod.loobia.entity.Order;
+import it.objectmethod.loobia.entity.OrderDetails;
+import it.objectmethod.loobia.repository.OrderRepository;
+
+@Component
+public class FileDownloadService {
+
+	@Autowired
+	private OrderRepository orderRepo;
+
+	@Value("${order.filename}")
+	private String fileNamePath;
+
+	public File generateFile(Integer id, HttpServletRequest request) {
+		Order order = orderRepo.findById(id).get();
+		File file = null;
+		if (order != null) {
+			DateFormat df = new SimpleDateFormat("ddMMyyyy");
+			String dataParse = df.format(order.getData());
+			String orderNumber = Integer.toString(order.getNumero());
+			String totAmount = amountGenerate(order.getImportoTot());
+			String totAmountDiscount = amountGenerate(order.getImportoTotScontato());
+			// aggiungo spazi vuoti se la lung non è suffic. e lo inserisco nel file
+			String customerCode = String.format("%-10s", order.getCustomerOrder().getCodiceCliente());
+			String customerBuisnessName = String.format("%-21s", order.getRagioneSocialeCliente());
+			String areaCode = String.format("%-4s", order.getCustomerOrder().getCodZona());
+			String finalOrderNumber = String.format("%-11s", orderNumber);
+
+			try {
+				String filePathName = this.fileNamePath.replace("NUMPH", orderNumber);
+				filePathName = filePathName.replace("ZONAPH", order.getArea().getCodZona());
+				filePathName = filePathName.replace("DATEPH", dataParse);
+				request.setAttribute("fileName", filePathName);
+
+				file = new File(filePathName);
+				FileWriter fw = new FileWriter(file.getAbsoluteFile());
+				BufferedWriter bw = new BufferedWriter(fw);
+				String finalDataForExtensionFile = String.format("%-11s", dataParse);
+
+				bw.write(customerCode + customerBuisnessName + areaCode + finalOrderNumber + finalDataForExtensionFile
+						+ totAmount + totAmountDiscount);
+				request.setAttribute("file", file);
+				// Codice relativo alla costruzione e inserimento delle righe dei dettagli
+				List<OrderDetails> listOrderDet = order.getDetailOrders();
+				for (OrderDetails orderDetails : listOrderDet) {
+					String idProd = Long.toString(orderDetails.getProduct().getId());
+					String descrizione = orderDetails.getProduct().getDescrizione();
+					String totPezzi = Integer.toString(orderDetails.getTotPezzi());
+					String singlePrice = amountGenerate(orderDetails.getPrezzoSingolo());
+					String convertedDiscount = discountConvert(orderDetails.getSconto());
+					String finalProductId = String.format("%-20s", idProd);
+					String finalTotPezzi = String.format("%-11s", totPezzi);
+					String finalDescrizione = String.format("%-41s", descrizione);
+					if (descrizione.length() > 40) {
+						finalDescrizione = descrizione.substring(0, 41);
+					}
+					bw.newLine();
+					bw.write(finalProductId + finalDescrizione + finalTotPezzi + singlePrice + convertedDiscount);
+				}
+				bw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return file;
+	}
+
+	private String amountGenerate(Float amount) {
+		String fixedPosAmount = null;
+		if (amount != null) {
+			String amountString = amount.toString();
+			amountString = amountString.replace(".", ",");
+			String[] nums = amountString.split(",");
+
+			String intPart = nums[0];
+			while (intPart.length() < 15) {
+				intPart = "0" + intPart;
+			}
+			if (nums.length > 1) {
+				String decPart = nums[1];
+				while (decPart.length() < 5) {
+					decPart = "0" + decPart;
+				}
+				fixedPosAmount = intPart + decPart;
+			} else {
+				fixedPosAmount = intPart + "00000";
+			}
+		}
+		return fixedPosAmount;
+	}
+
+	private String discountConvert(Integer discount) {
+		String scontoString = null;
+		if (discount != null) {
+			scontoString = Integer.toString(discount);
+			while (scontoString.length() < 3) {
+				scontoString = "0" + scontoString;
+			}
+		}
+		return scontoString;
+	}
+
+}
